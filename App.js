e995a328d6b450c5747f1e313c7d2773d1698fe9
mikerosa <rosa.mike.1982@gmail.com>
2020-03-24 13:12:15 -0400

    study of app gol

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('old text')
+
   return (
-    <View style={styles.container}>     
-      <Text>{ outputText }</Text>
-      <Button title="change text" onPress={() => setOutputText('new text')}/>
+    <View style={styles.screen}>
+      <View style={styles.inpuContainer}>
+        <TextInput
+          placeholder="Course Gol"
+          style={styles.input} />
+        <Button title="ADD" />
+      </View>
+      <View>
+        <Text>Here secund part</Text>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inpuContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: '80%',
+    borderBottomColor: 'blue',
+    borderBottomWidth: 1,
+    padding:10
+  }
 });
